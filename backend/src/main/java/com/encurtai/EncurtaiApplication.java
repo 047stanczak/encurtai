44996ecbb53b6dafc4c4376a48c2b471cf338fe1
047stanczak <119351507+047stanczak@users.noreply.github.com>
2025-12-10 19:39:10 -0300

    build📦 : primeiro commit com configuração de dependências

---
+++ backend/src/main/java/com/encurtai/EncurtaiApplication.java
@@ -0,0 +1,13 @@
+package com.encurtai;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EncurtaiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EncurtaiApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/encurtai/EncurtaiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.encurtai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EncurtaiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
